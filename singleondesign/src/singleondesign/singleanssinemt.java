70e0975e0a2b60614e82323489f292a004a990ad
surendr dhoni <surendr dhoni@DESKTOP-FJQNA4E>
2022-02-24 12:28:26 +0530

    assinment5

--- singleondesign/src/singleondesign/singleanssinemt.java
+++ singleondesign/src/singleondesign/singleanssinemt.java
@@ -1,30 +1,3 @@
-import java.io.*;
-		import java.util.*;
-		import java.text.*;
-		import java.math.*;
-		import java.util.regex.*;
-		import java.lang.reflect.*
-package singleondesign;
 
-public class singleanssinemt {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		public class singleanssinemt {
-			private static singleanssinemt instance;
-
-			private  singleanssinemt() {}
-			public  String str;
-			public static singleanssinemt getSingleinstance()
-			{
-				if(instance==null)
-				{
-				instance=new singleanssinemt();
-			}
-			return instance;
-		}
-		}
-		}
-	}
 
 
